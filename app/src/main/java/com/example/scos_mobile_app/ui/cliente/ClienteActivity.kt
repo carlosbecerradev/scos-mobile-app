770d56696b6b7c6b9490ebb7e39972ee2bf9d4ce
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2022-01-30 17:23:18 -0500

    :hammer: Agregando la barra de navegación inferior y sus fragmentos.

--- app/src/main/java/com/example/scos_mobile_app/ui/cliente/ClienteActivity.kt
+++ app/src/main/java/com/example/scos_mobile_app/ui/cliente/ClienteActivity.kt
@@ -2,11 +2,34 @@ package com.example.scos_mobile_app.ui.cliente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.scos_mobile_app.R
+import com.example.scos_mobile_app.databinding.ActivityClienteBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ClienteActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityClienteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cliente)
+        binding = ActivityClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomClienteNavigationView
+
+        val navController = findNavController(R.id.fragmentContainerView2)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeClienteFragment, R.id.nuevaOrdenFragment, R.id.encuestaFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
