6716dce42fedeceb1816eebb956d7ee3771a0072
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2022-01-31 00:26:42 -0500

    :hammer: Agregando barra de navegación superior para cerrar sesión.

--- app/src/main/java/com/example/scos_mobile_app/ui/cliente/ClienteActivity.kt
+++ app/src/main/java/com/example/scos_mobile_app/ui/cliente/ClienteActivity.kt
@@ -2,12 +2,18 @@ package com.example.scos_mobile_app.ui.cliente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.scos_mobile_app.R
 import com.example.scos_mobile_app.databinding.ActivityClienteBinding
+import com.example.scos_mobile_app.ui.auth.AuthActivity
+import com.example.scos_mobile_app.ui.startNewActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ClienteActivity : AppCompatActivity() {
@@ -31,5 +37,24 @@ class ClienteActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater =menuInflater
+        inflater.inflate(R.menu.top_menu_user, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_logout -> {
+                Toast.makeText(this, "Cerrando la sesión!", Toast.LENGTH_SHORT).show()
+                startNewActivity(AuthActivity::class.java)
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
