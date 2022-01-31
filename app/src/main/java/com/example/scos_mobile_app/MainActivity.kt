9071e872da15c4b063c736d2e2333010d8c6c47d
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2022-01-31 18:17:45 -0500

    :hammer: Escondiendo la barra de acción de Actividad Main.

--- app/src/main/java/com/example/scos_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/scos_mobile_app/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
         val userPreferences = UserPreferences(this)
 
